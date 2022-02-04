9853890126ea48521a3f27076b2ec08d910f80aa
dsamdave <virtualsavvy.nigeria@gmail.com>
2022-02-04 09:19:10 +0100

    server

---
+++ README.md
@@ -0,0 +1,13 @@
+## Install dependencies for server
+
+### `npm install`
+
+## Connect to your mongodb and add info in .env
+
+## Run the Express server only
+
+### `npm run dev`
+
+## Server runs on http://localhost:5000
+
+### Interface
---
+++ server/routes/blogRouter.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import blogCtrl from '../controllers/blogCtrl'
+import auth from '../middleware/auth'
+
+const router = express.Router()
+
+
+router.post('/blog', auth, blogCtrl.createBlog)
+
+router.get('/home/blogs', blogCtrl.getHomeBlogs)
+
+router.get('/blogs/:category_id', blogCtrl.getBlogsByCategory)
+
+
+export default router;
